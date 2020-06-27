aeeb80a2127932896ccd147b32fbd1c6e876bef4
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-06-27 16:18:43 +0300

    add preloading indicator init local store

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,12 +1,29 @@
 <script>
 	import {stores} from "@sapper/app"
 
-	import NavBar from "../components/top_navbar.svelte"
 	import BottomNavBar from "../components/bottom_navbar.svelte"
+	import NavBar from "../components/top_navbar.svelte"
+	import PreloadingIndicator from "../components/PreloadingIndicator.svelte"
+	import {light_mode, token, user} from "../store"
 
-	const {page} = stores()
+	if (typeof window !== "undefined") {
+		light_mode.use_local_storage()
+		token.use_local_storage()
+		user.use_local_storage()
+		if ($light_mode) {
+			document.documentElement.classList.remove("mode-dark")
+		} else {
+			document.documentElement.classList.add("mode-dark")
+		}
+	}
+
+	const {preloading, page} = stores()
 	$: path = $page.path
 </script>
+
+{#if $preloading}
+	<PreloadingIndicator />
+{/if}
 
 <NavBar />
 
